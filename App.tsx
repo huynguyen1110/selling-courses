145a603f8996e1576168eaf944a9b3263bd7cba3
huy nguyen <huynn@hivetech.vn>
2023-09-13 21:11:53 +0700

    init commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
