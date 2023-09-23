56af961e4bd3bbe99bd2dde3d44ba82487d08102
huy nguyen <huynn@hivetech.vn>
2023-09-23 09:19:58 +0700

    .

--- SellingCoursesAPI/SellingCourses/Program.cs
+++ SellingCoursesAPI/SellingCourses/Program.cs
@@ -102,11 +102,14 @@ builder.Services.AddScoped<IUserServices, UserServies>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
